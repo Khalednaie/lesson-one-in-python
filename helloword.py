ed3798ad138d64549ac35faf01df3aed7d6d7fff
Khalednaie <abo.naie.93@gmail.com>
2021-08-07 17:26:50 +0300

    func in python

--- helloword.py
+++ helloword.py
@@ -1,10 +1,32 @@
-print('khaled-python')
+# print('khaled-python')
 
-if 5>2 :
- print('five is greater than two ')
+# if 5>2 :
+#  print('five is greater than two ')
 
 
-x=5
-y=3
-s= 'Hello,Word'
-print(x+y,s)
+# x=5
+# y=3
+# s= 'Hello,Word'
+# print(x+y,s)
+
+# a=str(4)
+# z=int(4)
+# c=float(4)
+
+# print(a,z,c)
+# print(type(a),type(z),type(c))
+
+# fos = ['banana','apple','ornge']
+# x,y,z=fos
+# print(x,y,z)
+
+
+def myfunc():
+    global A,C,B
+    A= "Khaled"
+    C=' '
+    B= "Ali"
+myfunc()    
+
+print('py',A+C+B)
+
